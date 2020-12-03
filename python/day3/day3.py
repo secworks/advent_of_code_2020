a226d4627221bb2f0505dd07f582bdbdaccf1462
Joachim Strömbergson <joachim@assured.se>
2020-12-03 10:46:05 +0100

    Adding correct solution to day3, problem 1.

--- python/day3/day3.py
+++ python/day3/day3.py
@@ -5,6 +5,7 @@
 # day3.py
 # -------
 # Solutions to Advent of Code 2020, day 3.
+# https://adventofcode.com/2020/day/3
 #
 #=======================================================================
 
@@ -23,12 +24,40 @@ def get_input(filename):
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
+def is_tree(forest, x, y):
+    width = len(forest[0])
+    row = forest[y]
+    if row[(x % width)] == "#":
+        return 1
+    else:
+        return 0
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def count_trees(forest, step_x, step_y):
+    count = 0
+    height = len(forest)
+    x = 0
+    y = 0
+
+    while y < height:
+        count += is_tree(forest, x, y)
+        x += step_x
+        y += step_y
+
+    return count
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
 def problem1(filename):
     print("Problem 1")
     print("---------")
 
-    my_input = get_input(filename)
-
+    my_forest = get_input(filename)
+    trees = count_trees(my_forest, 3, 1)
+    print("Number of trees encountered: %d" % (trees))
     print("")
 
 
