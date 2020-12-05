859debcf28e6119a0f614cd7604a75b0b472ecb9
Joachim Strömbergson <joachim@assured.se>
2020-12-05 23:05:08 +0100

    Adding correct solution for day 5, problrm 1 and 2.

--- python/day5/day5.py
+++ python/day5/day5.py
@@ -11,6 +11,7 @@
 
 import sys
 
+TEST = True
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
@@ -21,6 +22,35 @@ def get_input(filename):
             l.append(line.strip())
     return l
 
+
+#-------------------------------------------------------------------
+# Given a seating code, calculate return the row and column.
+#-------------------------------------------------------------------
+def get_row_column_id(seating):
+    row_distance = 64
+    row = 0
+    column_distance = 4
+    column = 0
+    for c in seating:
+        if c == "F":
+            row_distance = int(row_distance / 2)
+
+        if c == "B":
+            row += row_distance
+            row_distance = int(row_distance / 2)
+
+        if c == "L":
+            column_distance = int(column_distance / 2)
+
+        if c == "R":
+            column += column_distance
+            column_distance = int(column_distance / 2)
+
+
+    return (row, column, (row * 8 + column))
+
+
+
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
 def problem1(filename):
@@ -28,9 +58,40 @@ def problem1(filename):
     print("---------")
 
     tickets = get_input(filename)
-    print(tickets)
+
+    # Tests with the given examples.
+    print("Seat (row, columnm id) for BFFFBBFRRR is:", get_row_column_id("BFFFBBFRRR"))
+    print("Seat (row, columnm id) for FFFBBBFRRR is:", get_row_column_id("FFFBBBFRRR"))
+    print("Seat (row, columnm id) for BBFFBBFRLL is:", get_row_column_id("BBFFBBFRLL"))
+
+    ms = (0, 0, 0)
+    for ticket in tickets:
+        (srow, scolumn, sid) = get_row_column_id(ticket)
+        if sid > ms[2]:
+            ms = (srow, scolumn, sid)
+
+    print("Seat with max id:", ms)
+    print("Max id: %d" % ms[2])
 
     print("")
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def occupied_seats(row):
+    seats = 0
+    for seat in row:
+        if seat == 1:
+            seats += 1
+    return seats
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def get_free_seat(row):
+    for i in range(8):
+        if row[i] == 0:
+            return i
 
 
 #-------------------------------------------------------------------
@@ -38,6 +99,21 @@ def problem1(filename):
 def problem2(filename):
     print("Problem 2")
     print("---------")
+
+    tickets = get_input(filename)
+
+    # Create an empty plane.
+    plane = [[0] * 8 for i in range(128)]
+
+    # Fill the plane with people.
+    for ticket in tickets:
+        (r, c, i) = get_row_column_id(ticket)
+        plane[r][c] = 1
+
+    for i in range(128):
+        if occupied_seats(plane[i]) == 7:
+            column = get_free_seat(plane[i])
+            print("id for my seat: %d" % (8 * i + column))
     print("")
 
 
