8be7df2d8496bf19b2b7239f4a05d9fd0bf96909
Joachim Strömbergson <joachim@assured.se>
2020-12-06 18:21:07 +0100

    Adding correct solutio for day four, problem one. Started to work on the complex rule checker for password field. Probably need to split it up into sub functions.

--- python/day4/day4.py
+++ python/day4/day4.py
@@ -23,6 +23,7 @@ def get_input(filename):
 
 
 #-------------------------------------------------------------------
+# Load the file and extract the raw ticket data.
 #-------------------------------------------------------------------
 def get_tickets(filename):
     print("Getting tickets from the input file %s" % filename)
@@ -44,15 +45,56 @@ def get_tickets(filename):
 
 
 #-------------------------------------------------------------------
+# Create a list of dicts, each dict corresponds to a ticket with
+# fields matching the key-value pairs in the ticket.
+#-------------------------------------------------------------------
+def get_db(tickets):
+    tickets_db = []
+    for ticket in tickets:
+        ticket_db = {}
+        for field in ticket:
+            (key, value) = field.split(":")
+            ticket_db.update({key:value})
+        tickets_db.append(ticket_db)
+    return tickets_db
+
+
+#-------------------------------------------------------------------
 #-------------------------------------------------------------------
 def problem1(filename):
     print("Problem 1")
     print("---------")
 
+    # Get tickets and parse into a list of dicts.
     tickets = get_tickets(filename)
-    for ticket in tickets:
-        print(ticket)
+    db = get_db(tickets)
+
+    valid = 0
+    for d in db:
+        if ((len(d.keys()) == 8) or
+            ((len(d.keys()) == 7) and "cid" not in d)):
+            valid += 1
+    print("Number of valid tickets: %d" % (valid))
     print("")
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def check_passport(d):
+    if (len(pdb.keys) < 7):
+        return 0
+
+    if ((len(d.keys()) == 7) and "cid" in d):
+        return 0
+
+    if (d['byr'] < 1920) or (d['byr'] > 2002):
+        return 0
+
+    if (d['iyr'] < 2010) or (d['iyr'] > 2020):
+        return 0
+
+    if (d['eyr'] < 2020) or (d['eyr'] > 2030):
+        return 0
 
 
 #-------------------------------------------------------------------
@@ -69,9 +111,9 @@ if __name__=="__main__":
     print("Advent of Code 2020, day 4")
     print("==========================")
 
-    problem1("day4_example.txt")
+    problem1("day4_input.txt")
 
-    problem2("day4_input.txt")
+    problem2("day4_example.txt")
 
     sys.exit(0)
 
