50a10b20475e424ca65fe072f7f34f8087b8a3d4
Joachim Strömbergson <joachim@assured.se>
2020-12-07 08:47:33 +0100

    Just committing the current status of the rust code.

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,11 +1,17 @@
-use std::env;
 use std::fs;
 
 fn main() {
     println!("AoC 2020, Day 1");
+    println!("===============");
 
-    let contents = fs::read_to_string("~/Sandbox/proj/ctf/advent_of_code_2020/rust/day1/src/day1_input.txt")
-    .expect("Something went wrong reading the file");
+ //   let data = fs::read_to_string("/Users/js/Sandbox/proj/ctf/advent_of_code_2020/rust/day1/src/day1_input.txt").expect("Unable to read file");
+    let data2 = fs::read("/Users/js/Sandbox/proj/ctf/advent_of_code_2020/rust/day1/src/day1_input.txt").expect("Unable to read file");
+//    println!("{}", data);
+//    println!("{}", data2.len());
 
-    println!("With text:\n{}", contents);
+    println!("Problem 1");
+    println!("---------");
+    println!("{}", data2[23]);
+
+
 }
