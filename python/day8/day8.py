b77c7b403afc9a3b8c34ee5679de90b90af9f315
Joachim Strömbergson <joachim@assured.se>
2020-12-08 08:20:20 +0100

    Adding correct solution for day8, problem 1.

--- python/day8/day8.py
+++ python/day8/day8.py
@@ -24,13 +24,61 @@ def get_input(filename):
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
+def get_code(filename):
+    raw = get_input(filename)
+    code = []
+    for line in raw:
+        (op, operand) = line.split(" ")
+        code.append((op, operand[0], int(operand[1:]), False))
+    return code
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
 def problem1(filename):
     print("Problem 1")
     print("---------")
 
     code = get_code(filename)
-
+    print(code)
     print("")
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def find_loop(code):
+    acc = 0
+    pc = 0
+    icnt = 0
+
+    while True:
+        (op, sign, operand, visited) = code[pc]
+        if visited:
+            return (icnt, acc, (op, sign, operand))
+
+        if op == "acc":
+            code[pc] = (op, sign, operand, True)
+            icnt += 1
+            pc += 1
+            if sign == "+":
+                acc += operand
+            else:
+                acc -= operand
+
+
+        if op == "jmp":
+            code[pc] = (op, sign, operand, True)
+            icnt += 1
+            if sign == "+":
+                pc = pc + operand
+            else:
+                pc = pc - operand
+
+
+        if op == "nop":
+            code[pc] = (op, sign, operand, True)
+            icnt += 1
+            pc += 1
 
 
 #-------------------------------------------------------------------
@@ -40,6 +88,9 @@ def problem2(filename):
     print("---------")
 
     code = get_code(filename)
+    (icnt, acc, instr) = find_loop(code)
+    print("Loop detected after %d instructions. Acc: %d" % (icnt, acc))
+    print("Instruction where loop occurs:", instr)
     print("")
 
 
