19205903dfff3647fdb509e7521c62185b51f996
Joachim Strömbergson <joachim@assured.se>
2020-12-09 13:32:24 +0100

    Adding correct solution to day 9, problem 1.

--- python/day9/day9.py
+++ python/day9/day9.py
@@ -18,8 +18,30 @@ def get_input(filename):
     l = []
     with open(filename,'r') as f:
         for line in f:
-            l.append(line.strip())
+            l.append(int(line.strip()))
     return l
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def get_sums(numlist):
+    sums = set()
+    js = 1
+    for i in range(len(numlist) - 1):
+        for j in range(js, len(numlist)):
+            sums.add(numlist[i] + numlist[j])
+        js += 1
+
+    return sums
+
+
+#-------------------------------------------------------------------
+# Given a preamble, check if the given value is a sum of two
+# values in the preamble.
+#-------------------------------------------------------------------
+def check_sum(val, preamble):
+    sums = get_sums(preamble)
+    return val in sums
 
 
 #-------------------------------------------------------------------
@@ -28,6 +50,13 @@ def problem1(filename):
     print("Problem 1")
     print("---------")
 
+    numbers = get_input(filename)
+    preamble = 25
+
+    i = 0
+    while check_sum(numbers[(i + preamble)], numbers[i:(i + preamble)]):
+        i += 1
+    print("The first number to not be a sum: %d" % numbers[(i + preamble)])
     print("")
 
 
