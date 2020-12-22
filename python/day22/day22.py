1fbd87177eb8275fdfacc0eda217cb58e1777b94
Joachim Strömbergson <joachim@assured.se>
2020-12-22 14:37:40 +0100

    Adding working solution for day22, problem one.

--- python/day22/day22.py
+++ python/day22/day22.py
@@ -32,14 +32,58 @@ def get_cards(raw):
 
     i = 1
     while raw[i] != "":
-        p1.append(raw[i])
+        p1.append(int(raw[i]))
         i += 1
 
     i += 2
     for l in raw[i:]:
-        p2.append(l)
+        p2.append(int(l))
 
     return (p1, p2)
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def play_game(p1, p2):
+    r = 1
+    while (len(p1) > 0) and (len(p2) > 0):
+        print("--- Round %d ---" % (r))
+        print("Player 1's deck:", p1)
+        print("Player 2's deck:", p2)
+
+        c1 = p1.pop(0)
+        c2 = p2.pop(0)
+        print("Player 1 plays %d" % (c1))
+        print("Player 2 plays %d" % (c2))
+
+        if c1 > c2:
+            p1.append(c1)
+            p1.append(c2)
+            print("Player 1 wins the round!")
+
+        else:
+            p2.append(c2)
+            p2.append(c1)
+            print("Player 2 wins the round!")
+
+        r += 1
+        print("")
+
+    if len(p1) > 0:
+        return (1, p1, (r - 1))
+    else:
+        return (2, p2, (r - 1))
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def get_score(cards):
+    s = 0
+    n = len(cards)
+    while len(cards) > 0:
+        s += cards.pop(0) * n
+        n -= 1
+    return s
 
 
 #-------------------------------------------------------------------
@@ -47,10 +91,19 @@ def get_cards(raw):
 def problem1(filename):
     print("Problem 1")
     print("---------")
+
+#    print("Testing game:")
+#    (player, cards, rounds) = play_game([9, 2, 6, 3, 1], [5, 8, 4, 7, 10])
+#    print(player, cards, rounds)
+#    print("Test score: %d" % (get_score(cards)))
+
     raw = get_input(filename)
     (p1, p2) = get_cards(raw)
-    print("player 1:", p1)
-    print("player 2:", p2)
+    (player, cards, rounds) = play_game(p1, p2)
+    score = get_score(cards)
+
+    print("Winner was player %d after %d rounds." % (player, rounds))
+    print("Final score was: %d" % (score))
     print("")
 
 
