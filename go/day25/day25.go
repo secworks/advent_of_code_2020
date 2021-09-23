ffc1533662661227f25b731e7611e07f25c09a0d
Joachim Strömbergson <joachim@assured.se>
2021-09-23 09:00:42 +0200

    Add solution for day25, problem 1

--- go/day25/day25.go
+++ go/day25/day25.go
@@ -1,21 +1,89 @@
+//======================================================================
+//
+// day25.go
+// --------
+// Solution to:
+// https://adventofcode.com/2020/day/25
+//
+//======================================================================
+
 package main
 
 import "fmt"
 
 
-func find_loop_size(pk int64, sn int64) {
+func find_loop_size(pk int64, sn int64) int64 {
 	var value int64 = 1
 	var loops int64 = 0
+
 	for (value != pk) {
 		value *= sn
 		value = value % 20201227
 		loops += 1
 	}
-	fmt.Printf("Loops: %d\n", loops)
+
+	return loops
+}
+
+
+func find_private_key(pk int64, ls int64) int64 {
+	var value int64 = 1
+	var i int64
+
+	for i = 0; i < ls; i++ {
+		value *= pk
+		value = value % 20201227
+	}
+
+	return value
+}
+
+
+// Run example values for problem 1.
+func example1() {
+	fmt.Println("Example 1:")
+	fmt.Println("----------")
+
+	var sn int64 = 7
+	var card_pk int64 = 5764801
+	var door_pk int64 = 17807724
+
+	var card_loops = find_loop_size(card_pk, sn)
+	var door_loops = find_loop_size(door_pk, sn)
+	fmt.Printf("Card loops: %v \n", card_loops)
+	fmt.Printf("Door loops: %v \n", door_loops)
+
+	var card_sk = find_private_key(door_pk, card_loops)
+	var door_sk = find_private_key(card_pk, door_loops)
+	fmt.Printf("Card secret key: %v \n", card_sk)
+	fmt.Printf("Door secret_key: %v \n", door_sk)
+	fmt.Println("")
+}
+
+
+func problem1() {
+	fmt.Println("Problem 1:")
+	fmt.Println("----------")
+
+	var sn int64 = 7
+	var card_pk int64 = 6929599
+	var door_pk int64 = 2448427
+
+	var card_loops = find_loop_size(card_pk, sn)
+	var door_loops = find_loop_size(door_pk, sn)
+	fmt.Printf("Card loops: %v \n", card_loops)
+	fmt.Printf("Door loops: %v \n", door_loops)
+
+	var card_sk = find_private_key(door_pk, card_loops)
+	var door_sk = find_private_key(card_pk, door_loops)
+	fmt.Printf("Card secret key: %v \n", card_sk)
+	fmt.Printf("Door secret_key: %v \n", door_sk)
+	fmt.Println("")
 }
 
 
 func main() {
-	fmt.Println("Problem 1:\n")
-	find_loop_size(5764801, 7)
+	fmt.Println("Solution for AoC 2020, Day 25.\n")
+	example1()
+	problem1()
 }
